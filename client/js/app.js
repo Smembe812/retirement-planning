cf7d28e40cc74b8616c639fae9df28e5cb0bd292
Smembe812 <psemberekajr@gmail.com>
2017-09-03 22:12:49 +0200

    added training programmes for individualsa page

--- client/js/app.js
+++ client/js/app.js
@@ -27,6 +27,16 @@ angular.module('app', [
         ],
         templateUrl: '../website/about.html'
       })
+      .state('website-services-training-programmes-for-individuals', {
+        url: '/website/services/training-programmes-for-individuals',
+        css: [
+          '../assets/css/bootstrap.min.css',
+          '../assets/css/animate.css',
+          '../assets/css/now-ui-kit.css',
+          '../assets/css/rpc.css'
+        ],
+        templateUrl: '../website/services.training-programmes-for-individuals.html'
+      })
       .state('website-services-will-preparation-and-execution', {
         url: '/website/services/will-preparation-and-execution',
         css: [
