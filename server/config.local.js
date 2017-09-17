7e086321888c18c919521682c6cc414d660f50b8
Smembe812 <psemberekajr@gmail.com>
2017-09-17 09:37:20 +0200

    configuring env viriables

--- server/config.local.js
+++ server/config.local.js
@@ -3,7 +3,7 @@
 // Node module: loopback-example-offline-sync
 // This file is licensed under the MIT License.
 // License text available at https://opensource.org/licenses/MIT
-/*
+
 'use strict';
 
 var GLOBAL_CONFIG = require('../global-config');
@@ -19,4 +19,3 @@ module.exports = {
   port: GLOBAL_CONFIG.port,
   legacyExplorer: GLOBAL_CONFIG.legacyExplorer
 };
-*/
